b350fa328c230ba987b2aac04ae0388486db150a
Eric Obermühlner <eobermuhlner@gmail.com>
2019-09-06 18:02:33 +0200

    provide javax.script.ScriptEngineFactory service in module-info

--- ch.obermuhlner.scriptengine.java/src/main/java/module-info.java
+++ ch.obermuhlner.scriptengine.java/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module ch.obermuhlner.scriptengine.java {
     exports ch.obermuhlner.scriptengine.java.name;
     exports ch.obermuhlner.scriptengine.java.util;
 
+    provides javax.script.ScriptEngineFactory with ch.obermuhlner.scriptengine.java.JavaScriptEngineFactory;
+
     requires transitive java.scripting;
 
     requires java.compiler;
